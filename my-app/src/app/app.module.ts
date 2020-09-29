796d9d3d2a2c30fd0cfe06c9004e93f972237f4b
kalpana RI <s16368@sci.pdn.ac.lk>
2020-09-29 22:56:20 +0530

    new commit

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//add from
+ import { AppBoostrapModule } from './app-boostrap/app-boostrap.module';
+
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppBoostrapModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
