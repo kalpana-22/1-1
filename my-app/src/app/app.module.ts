66cece73e2e6aebade4d0c7c7bff549abcbaa3be
kalpana RI <s16368@sci.pdn.ac.lk>
2020-09-30 13:07:34 +0530

    1

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
